a316f18a159a9c306fb797056f4c6e43695d0a23
brianlin725 <brianlin22987@gmail.com>
2022-11-04 01:48:19 -0500

    Created a Treeview as well as buttons

--- Item.py
+++ Item.py
@@ -1,16 +1,16 @@
 class Item():
-    def __init__(self, name, price, unit, people=None):
+    def __init__(self, name, price, quantity, people=None):
         if people is None:
             people = []
         self.name = name
         self.price = price
-        self.unit = unit
+        self.quantity = quantity
         self.people = people
 
     # equals based on all properties
     def __eq__(self, other):
         return self.name == other.name and self.price == other.price \
-               and self.unit == other.unit and self.people == other.people
+               and self.quantity == other.quantity and self.people == other.people
 
     # get methods
     def getName(self):
@@ -19,8 +19,8 @@ class Item():
     def getPrice(self):
         return self.price
 
-    def getUnit(self):
-        return self.unit
+    def getQuantity(self):
+        return self.quantity
 
     def getPeople(self):
         return self.people
@@ -36,6 +36,6 @@ class Item():
     def setPrice(self, price):
         self.price = price
 
-    def setUnit(self, unit):
-        self.unit = unit
+    def Quantity(self, quantity):
+        self.quantity = quantity
 
